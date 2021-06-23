90862c77ffb5f8278c90d9cd22205b2e9adf9fbe
jahelj29 <51664931+jahelj29@users.noreply.github.com>
2021-06-23 13:28:30 -0600

    avance ropa 2

--- src/components/ClothesAdmin.js
+++ src/components/ClothesAdmin.js
@@ -3,17 +3,21 @@ function ClothesAdmin() {
     <div className="col-12 justify-content-center d-flex mb-3">
       <div className="mt-2">
         <div className="card">
-          <img
-            src="https://www.ago2.com/wp-content/uploads/2017/07/INTERSPORT_06.jpg"
-            className="card-img-top"
-          />
           <div className="card-body">
             <h5 className="card-title text-center">Nuevo producto</h5>
-            <p className="card-text text-center">
-              Debe rellenar los espacios de información del producto
-            </p>
-            <div className="text-center">
-                <button className="btn btn-primary px-5">Agregar imagen</button>
+            <div className="mt-4">
+              <p className="card-text text-center">
+                Debe rellenar los espacios de información del producto
+              </p>
+            </div>
+            <div className="text-center mt-4">
+              <img
+                src="https://www.ago2.com/wp-content/uploads/2017/07/INTERSPORT_06.jpg"
+                className="card-img-top"
+              />
+              <div className = "mt-3">
+              <button className="btn btn-primary px-5">Agregar imagen</button>
+              </div>
             </div>
           </div>
         </div>
