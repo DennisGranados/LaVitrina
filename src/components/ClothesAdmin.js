aef145755befd506b670ac7eca17f483847ed02c
jahelj29 <51664931+jahelj29@users.noreply.github.com>
2021-06-23 18:15:16 -0600

    intento de campo de texto

--- src/components/ClothesAdmin.js
+++ src/components/ClothesAdmin.js
@@ -10,13 +10,22 @@ function ClothesAdmin() {
                 Debe rellenar los espacios de información del producto
               </p>
             </div>
+            <div class="input-group sm-2">
+              <div class="input-group-prepend">
+                <span class="input-group-text">Nombre</span>
+              </div>
+              <textarea
+                class="form-control"
+                aria-label="With textarea"
+              ></textarea>
+            </div>
             <div className="text-center mt-4">
               <img
                 src="https://www.ago2.com/wp-content/uploads/2017/07/INTERSPORT_06.jpg"
                 className="card-img-top"
               />
-              <div className = "mt-3">
-              <button className="btn btn-primary px-5">Agregar imagen</button>
+              <div className="mt-3">
+                <button className="btn btn-primary px-5">Agregar imagen</button>
               </div>
             </div>
           </div>
