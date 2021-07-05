5b9ca103febd8d1da1aff34448ca11c1916687f4
Denilson Granados Solano <denilson.granados@ucrso.info>
2021-07-05 15:10:22 -0600

    financeAdmin style completo

--- src/components/FinanceAdmin.js
+++ src/components/FinanceAdmin.js
@@ -2,59 +2,40 @@ import { Link } from "react-router-dom";
 
 function FinanceAdmin() {
   return (
-    <div>
-      <div className="col-12 justify-content-center d-flex mb-3">
-        <div className="col-6 text-center">
-          <h1>Métodos de pago</h1>
-        </div>
-        <div className="col-6 text-center">
-          <h1>Estadísticas</h1>
-        </div>
-      </div>
-      <div className="col-12 d-flex mb-3 mx-2">
-        <div className="col-6 m-auto d-flex">
-          <div className="col-6 m-auto">
-            <div className="card shadowCards">
-              <div className="card-body">
-                <h5 className="card-title text-center">
-                  Gestionar métodos de pago
-                </h5>
-                <p className="card-text text-justify">
-                  Añade o elimina métodos de pago.
-                </p>
-                <div className="text-center">
-                  <Link
-                    className="btn btnAccept"
-                    to="/admin/finance/adminPayment"
-                  >
-                    Ir a la sección
-                  </Link>
-                </div>
-              </div>
+    <div className="orderCards">
+      <div className="my-2">
+        <h1 className="text-center">Métodos de pago</h1>
+        <div className="card shadowCards">
+          <div className="card-body">
+            <h5 className="card-title text-center">
+              Gestionar métodos de pago
+            </h5>
+            <p className="card-text text-justify">
+              Añade o elimina métodos de pago.
+            </p>
+            <div className="text-center">
+              <Link className="btn btnAccept" to="/admin/finance/adminPayment">
+                Ir a la sección
+              </Link>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="vl text-center me-1"></div>
-        </div>
-        <div className="col-6 d-flex">
-          <div className="col-6 m-auto">
-            <div className="card shadowCards">
-              <div className="card-body">
-                <h5 className="card-title text-center">
-                  Ver finanzas de la tienda
-                </h5>
-                <p className="card-text text-justify">
-                  Añade nuevas prendas, correspondientes a un estilo existente.
-                </p>
-                <div className="text-center">
-                  <Link
-                    className="btn btnAccept"
-                    to="/admin/inventory/add-item"
-                  >
-                    Ir a la sección
-                  </Link>
-                </div>
+      </div>
+      <div>
+        <div className="my-2">
+          <h1 className="text-center">Estadísticas</h1>
+          <div className="card shadowCards">
+            <div className="card-body">
+              <h5 className="card-title text-center">
+                Ver finanzas de la tienda
+              </h5>
+              <p className="card-text text-justify">
+                Añade nuevas prendas, correspondientes a un estilo existente.
+              </p>
+              <div className="text-center">
+                <Link className="btn btnAccept" to="/admin/inventory/add-item">
+                  Ir a la sección
+                </Link>
               </div>
             </div>
           </div>
