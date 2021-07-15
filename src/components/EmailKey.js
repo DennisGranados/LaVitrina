3e4b155fbb735a6c15b95b3616bb822ef8c8ed86
Denilson Granados Solano <denilson.granados@ucrso.info>
2021-07-14 19:57:17 -0600

    EmailKey Styles Completo

--- src/components/EmailKey.js
+++ src/components/EmailKey.js
@@ -25,15 +25,30 @@ function ContactUs() {
   }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="orderCards">
+      <div className="card my-3">
+        <div className="card-body text-center">
+          <h4 className="text-center mb-4">Envio de correo</h4>
+          <form className="contact-form" onSubmit={sendEmail}>
+            <div>
+              <label className="form-label">Nombre</label>
+              <input className="form-control" type="text" name="user_name" />
+            </div>
+            <div>
+              <label className="form-label mt-3">Correo Electrónico</label>
+              <input className="form-control" type="email" name="user_email" />
+            </div>
+            <div>
+              <label className="form-label ">Mensaje</label>
+              <textarea className="form-control" name="message" />
+            </div>
+            <div className="text-center mt-3">
+              <input className="btnAccept btn" type="submit" value="Enviar" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 export default ContactUs;
